49cebbb4324dd8dbc11f4339e868c5376142ecb7
porrinha09 <142546604+porrinha09@users.noreply.github.com>
2024-01-15 14:52:59 -0300

    Update Synapse x.lua

--- Main/Synapse x.lua
+++ Main/Synapse x.lua
@@ -1,5 +1,5 @@
 local synversion = "2.0.0"
-
+end
     local HttpService = game:GetService("HttpService")
     local response = game:HttpGet("https://api.whatexploitsare.online/status")
     local data = HttpService:JSONDecode(response)
